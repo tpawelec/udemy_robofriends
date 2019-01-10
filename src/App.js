3996c30baa488f285f4fa8b0afaee578e247bb2b
Tomasz Pawelec <tpawelec@outlook.com>
2019-01-10 22:43:29 +0100

    Lesson 1-3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+    onSearchChange = (evt) => {
+        this.setState({searchfield : evt.target.value});
+    }
+    render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filterRobots}/>
+            </div>
+        );
+    }
+    
 }
 
 export default App;
